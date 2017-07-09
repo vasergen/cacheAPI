d9f74d5cc79621d327cd43679cfae64e3864e7ae
vasergen <vasergen@gmail.com>
2017-07-09 14:51:31 +0200

    add server start function

--- app/api.js
+++ app/api.js
@@ -1,4 +1,13 @@
 'use strict'
 
 const logger = require('./service/logger')
+const server = require('./server')
 logger.info('START API ...')
+
+server.start()
+    .then(() => {
+        logger.info('API STARTED SUCCESSFULLY')
+    })
+    .catch((err) => {
+        logger.error(err)
+    })
--- app/server.js
+++ app/server.js
@@ -1,5 +1,24 @@
 'use strict'
 
+const express = require('express')
+const app = express()
+const logger = require('./service/logger')
 const mongo = require('./service/mongo')
+const appRoutes = require('./routes')
+const config = require('config')
+const blubird = require('bluebird')
+blubird.promisify(app.listen)
 
-mongo.connect()
+app.use(appRoutes)
+
+const start = () => {
+    return Promise.resolve()
+        .then(() => {
+            return mongo.connect()
+        })
+        .then(() => {
+            return app.listen(3000) // TODO: move to config
+        })
+}
+
+module.exports.start = start
